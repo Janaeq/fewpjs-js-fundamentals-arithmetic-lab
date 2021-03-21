4d01be16efec68c208d14464ddbb1e8153ee6a19
Janae Quinones <janaemassa@gmail.com>
2021-03-21 01:52:18 -0400

    done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+// problem 1
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+// problem 2
+const random = Math.floor(Math.random() * 10) + 1
+
+// problem 3
+const num3 = 28
+const num4 = 7
+const mod = num3 / num4
+
+// problem 4
+const max = Math.max(1, 2, 3, 6, 8, 19, 20)
